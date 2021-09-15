bf40e02be45d26530d5245f20bbcbf6d6f517b23
Turker Teke <turker.teke@i4.com.tr>
2021-09-15 11:39:03 +0300

    report translated to turkish

--- opensfm/report.py
+++ opensfm/report.py
@@ -137,7 +137,7 @@ class Report:
         # title
         self.pdf.set_font("Helvetica", "B", self.title_size)
         self.pdf.set_text_color(*self.mapi_light_green)
-        self.pdf.cell(0, self.margin, "Quality Report", align="C")
+        self.pdf.cell(0, self.margin, "Kalite Raporu", align="C")
         self.pdf.set_xy(self.margin, self.title_size)
 
         # version number
@@ -160,22 +160,22 @@ class Report:
         self.pdf.set_xy(self.margin, self.pdf.get_y() + 2 * self.margin)
 
     def make_dataset_summary(self):
-        self._make_section("Dataset Summary")
+        self._make_section("Veri Özeti")
 
         rows = [
             #["Dataset", self.dataset_name],
-            ["Date", self.stats["processing_statistics"]["date"]],
+            ["Tarih", self.stats["processing_statistics"]["date"]],
             [
-                "Area Covered",
+                "Taranan Alan",
                 f"{self.stats['processing_statistics']['area']/1e6:.6f} km²",
             ],
             [
-                "Processing Time",
+                "İşlem Süresi",
                 #f"{self.stats['processing_statistics']['steps_times']['Total Time']:.2f} seconds",
                 self.stats['odm_processing_statistics']['total_time_human'],
             ],
-            ["Capture Start", self.stats["processing_statistics"]["start_date"]],
-            ["Capture End", self.stats["processing_statistics"]["end_date"]],
+            ["Uçuş Başlangıç", self.stats["processing_statistics"]["start_date"]],
+            ["Uçuş Bitiş", self.stats["processing_statistics"]["end_date"]],
         ]
         self._make_table(None, rows, True)
         self.pdf.set_xy(self.margin, self.pdf.get_y() + self.margin)
@@ -187,7 +187,7 @@ class Report:
         )
 
     def make_processing_summary(self):
-        self._make_section("Processing Summary")
+        self._make_section("Analiz Özeti")
 
         rec_shots, init_shots = (
             self.stats["reconstruction_statistics"]["reconstructed_shots_count"],
@@ -202,55 +202,55 @@ class Report:
         if self.stats["reconstruction_statistics"]["has_gps"]:
             geo_string.append("GPS")
         if self._has_meaningful_gcp():
-            geo_string.append("GCP")
+            geo_string.append("YKN")
 
         ratio_shots = rec_shots / init_shots * 100 if init_shots > 0 else -1
         rows = [
             [
-                "Reconstructed Images",
-                f"{rec_shots} over {init_shots} shots ({ratio_shots:.1f}%)",
+                "Kullanılan Fotoğraflar",
+                f"{init_shots}'da  {rec_shots} adet ({ratio_shots:.1f}%)",
             ],
             [
-                "Reconstructed Points (Sparse)",
-                f"{rec_points} over {init_points} points ({rec_points/init_points*100:.1f}%)",
+                "Oluşturulan/Kullanılan Noktalar (Seyrek)",
+                f"{init_points}'da {rec_points} nokta ({rec_points/init_points*100:.1f}%)",
             ],
             # [
             #     "Reconstructed Components",
             #     f"{self.stats['reconstruction_statistics']['components']} component",
             # ],
             [
-                "Detected Features",
-                f"{self.stats['features_statistics']['detected_features']['median']:,} features",
+                "Tespit Edilen Öznitelikler",
+                f"{self.stats['features_statistics']['detected_features']['median']:,} öznitelik",
             ],
             [
-                "Reconstructed Features",
-                f"{self.stats['features_statistics']['reconstructed_features']['median']:,} features",
+                "İnşada kullanılan Öznitelikler",
+                f"{self.stats['features_statistics']['reconstructed_features']['median']:,} öznitelik",
             ],
-            ["Geographic Reference", " and ".join(geo_string)],
+            ["Coğrafi Referanslama", " and ".join(geo_string)],
         ]
 
         # Dense (if available)
         if self.stats.get('point_cloud_statistics'):
             if self.stats['point_cloud_statistics'].get('dense'):
                 rows.insert(2, [
-                    "Reconstructed Points (Dense)",
-                    f"{self.stats['point_cloud_statistics']['stats']['statistic'][0]['count']:,} points"
+                    "Oluşturulan Noktalar (Yoğun)",
+                    f"{self.stats['point_cloud_statistics']['stats']['statistic'][0]['count']:,} nokta"
                 ])
 
         # GSD (if available)
         if self.stats['odm_processing_statistics'].get('average_gsd'):
             rows.insert(3, [
-                "Average Ground Sampling Distance (GSD)",
+                "Ortalama Yersel Örnekleme Mesafesi (GSD)",
                 f"{self.stats['odm_processing_statistics']['average_gsd']:.1f} cm"
             ])
 
-        row_gps_gcp = [" / ".join(geo_string) + " errors"]
+        row_gps_gcp = [" / ".join(geo_string) + " hataları"]
         geo_errors = []
         if self.stats["reconstruction_statistics"]["has_gps"]:
             geo_errors.append(f"{self.stats['gps_errors']['average_error']:.2f}")
         if self._has_meaningful_gcp():
             geo_errors.append(f"{self.stats['gcp_errors']['average_error']:.2f}")
-        row_gps_gcp.append(" / ".join(geo_errors) + " meters")
+        row_gps_gcp.append(" / ".join(geo_errors) + " m")
         rows.append(row_gps_gcp)
 
         self._make_table(None, rows, True)
@@ -267,28 +267,32 @@ class Report:
         self.pdf.set_xy(self.margin, self.pdf.get_y() + self.margin)
 
     def make_processing_time_details(self):
-        self._make_section("Processing Time Details")
+        self._make_section("Analiz Süresi Detayı")
 
         columns_names = list(self.stats["processing_statistics"]["steps_times"].keys())
         formatted_floats = []
         for v in self.stats["processing_statistics"]["steps_times"].values():
-            formatted_floats.append(f"{v:.2f} sec.")
+            formatted_floats.append(f"{v:.2f} sn.")
         rows = [formatted_floats]
         self._make_table(columns_names, rows)
         self.pdf.set_xy(self.margin, self.pdf.get_y() + 2 * self.margin)
 
     def make_gps_details(self):
-        self._make_section("GPS/GCP/3D Errors Details")
+        self._make_section("GPS/YKN/3D Hata Detayı")
 
         # GPS
         table_count = 0
         for error_type in ["gps", "gcp", "3d"]:
             rows = []
-            columns_names = [error_type.upper(), "Mean", "Sigma", "RMS Error"]
+            if error_type = "gps" :
+                columns_names = ["YKN", "Mean", "Sigma", "Ortalama Karekök Hata"]
+            else :
+                columns_names = [error_type.upper(), "Mean", "Sigma", "Ortalama Karekök Hata"]
+
             if "average_error" not in self.stats[error_type + "_errors"]:
                 continue
             for comp in ["x", "y", "z"]:
-                row = [comp.upper() + " Error (meters)"]
+                row = [comp.upper() + " Hata (metre)"]
                 row.append(f"{self.stats[error_type + '_errors']['mean'][comp]:.3f}")
                 row.append(f"{self.stats[error_type +'_errors']['std'][comp]:.3f}")
                 row.append(f"{self.stats[error_type +'_errors']['error'][comp]:.3f}")
@@ -296,7 +300,7 @@ class Report:
 
             rows.append(
                 [
-                    "Total",
+                    "Toplam",
                     "",
                     "",
                     f"{self.stats[error_type +'_errors']['average_error']:.3f}",
@@ -317,11 +321,11 @@ class Report:
             rows = []
             if a_ce90 > 0 and a_le90 > 0:
                 rows += [[
-                    "Horizontal Accuracy CE90 (meters)",
+                    "Yatayda Doğruluk CE90 (metre)",
                     f"{a_ce90:.3f}",
                     f"{r_ce90:.3f}" if r_ce90 > 0 else "-",
                 ],[
-                    "Vertical Accuracy LE90 (meters)",
+                    "Düşeyde Doğruluk LE90 (metre)",
                     f"{a_le90:.3f}",
                     f"{r_le90:.3f}" if r_le90 > 0 else "-",
                 ]]
@@ -329,13 +333,13 @@ class Report:
             if rows:
                 if table_count > 2:
                     self.add_page_break()
-                self._make_table(["", "Absolute", "Relative"], rows, True)
+                self._make_table(["", "Mutal", "Göreceli"], rows, True)
                 self.pdf.set_xy(self.margin, self.pdf.get_y() + self.margin / 2)
 
         self.pdf.set_xy(self.margin, self.pdf.get_y() + self.margin / 2)
 
     def make_features_details(self):
-        self._make_section("Features Details")
+        self._make_section("Öznitelik Detayları")
 
         heatmap_height = 60
         heatmaps = [
@@ -347,10 +351,14 @@ class Report:
         if len(heatmaps) > 1:
             logger.warning("Please implement multi-model display")
 
-        columns_names = ["", "Min.", "Max.", "Mean", "Median"]
+        columns_names = ["", "Min.", "Max.", "Ortalama", "Medyan"]
         rows = []
+        translations = {
+            "Detected": "Tespit Edilen",
+            "Reconstructed": "İnşa Edilen"
+        }
         for comp in ["detected_features", "reconstructed_features"]:
-            row = [comp.replace("_", " ").replace("features", "").capitalize()]
+            row = [translations[comp.replace("_", " ").replace("features", "").capitalize()]]
             for t in columns_names[1:]:
                 row.append(
                     f"{self.stats['features_statistics'][comp][t.replace('.', '').lower()]:.0f}"
@@ -361,11 +369,11 @@ class Report:
         self.pdf.set_xy(self.margin, self.pdf.get_y() + self.margin)
 
     def make_reconstruction_details(self):
-        self._make_section("Reconstruction Details")
+        self._make_section("İnşa Detayları")
 
         rows = [
             [
-                "Average Reprojection Error (normalized / pixels / angular)",
+                "Ortalama Yansıtma Hatası (normalize / pixel / açısal)",
                 (
                     f"{self.stats['reconstruction_statistics']['reprojection_error_normalized']:.2f} / "
                     f"{self.stats['reconstruction_statistics']['reprojection_error_pixels']:.2f} / "
@@ -373,12 +381,12 @@ class Report:
                 ),
             ],
             [
-                "Average Track Length",
-                f"{self.stats['reconstruction_statistics']['average_track_length']:.2f} images",
+                "Ortalama Hat Uzunluğu",
+                f"{self.stats['reconstruction_statistics']['average_track_length']:.2f} fotoğraf",
             ],
             [
-                "Average Track Length (> 2)",
-                f"{self.stats['reconstruction_statistics']['average_track_length_over_two']:.2f} images",
+                "Ortalama Hat Uzunluğu (> 2)",
+                f"{self.stats['reconstruction_statistics']['average_track_length_over_two']:.2f} fotoğraf",
             ],
         ]
         self._make_table(None, rows, True)
@@ -397,7 +405,7 @@ class Report:
         self.pdf.set_xy(self.margin, self.pdf.get_y() + self.margin)
 
     def make_camera_models_details(self):
-        self._make_section("Camera Models Details")
+        self._make_section("Kamera Modeli Detayları")
 
         for camera, params in self.stats["camera_errors"].items():
             residual_grids = [
@@ -413,8 +421,8 @@ class Report:
             names = [""] + list(initial.keys())
 
             rows = []
-            rows.append(["Initial"] + [f"{x:.4f}" for x in initial.values()])
-            rows.append(["Optimized"] + [f"{x:.4f}" for x in optimized.values()])
+            rows.append(["Başlangıç"] + [f"{x:.4f}" for x in initial.values()])
+            rows.append(["Optimize"] + [f"{x:.4f}" for x in optimized.values()])
 
             self._make_subsection(camera)
             self._make_table(names, rows)
@@ -469,7 +477,7 @@ class Report:
             self.pdf.set_xy(self.margin, self.pdf.get_y() + self.margin / 2)
 
     def make_tracks_details(self):
-        self._make_section("Tracks Details")
+        self._make_section("Hat Detayları")
         matchgraph_height = 80
         matchgraph = [
             f
@@ -482,12 +490,12 @@ class Report:
 
         histogram = self.stats["reconstruction_statistics"]["histogram_track_length"]
         start_length, end_length = 2, 10
-        row_length = ["Length"]
+        row_length = ["Uzaklık(Fotoğraf)"]
         for length, _ in sorted(histogram.items(), key=lambda x: int(x[0])):
             if int(length) < start_length or int(length) > end_length:
                 continue
             row_length.append(length)
-        row_count = ["Count"]
+        row_count = ["Adet"]
         for length, count in sorted(histogram.items(), key=lambda x: int(x[0])):
             if int(length) < start_length or int(length) > end_length:
                 continue
@@ -501,7 +509,7 @@ class Report:
         self.pdf.add_page("P")
 
     def make_survey_data(self):
-        self._make_section("Survey Data")
+        self._make_section("Örtüşme Detayı (Fotoğraf Sayısı)")
 
         self._make_centered_image(
             os.path.join(self.output_path, "overlap.png"), 90
@@ -525,20 +533,20 @@ class Report:
         count = 0
 
         if os.path.isfile(ortho) or os.path.isfile(dsm):
-            self._make_section("Previews")
+            self._make_section("Önizlemeler")
             
             if os.path.isfile(ortho):
                 self._make_centered_image(
                     os.path.join(self.output_path, ortho), 110
                 )
-                self._add_image_label("Orthophoto")
+                self._add_image_label("Ortofoto")
                 count += 1
 
             if os.path.isfile(dsm) and self.stats.get('dsm_statistics'):
                 self._make_centered_image(
                     os.path.join(self.output_path, dsm), 110
                 )
-                self._add_image_label("Digital Surface Model")
+                self._add_image_label("Dijital Yüzey Modeli (DSM)")
 
                 self._make_centered_image(
                     os.path.join(self.output_path, "dsm_gradient.png"), 4
@@ -557,7 +565,7 @@ class Report:
                 self._make_centered_image(
                     os.path.join(self.output_path, dtm), 110
                 )
-                self._add_image_label("Digital Terrain Model")
+                self._add_image_label("Dijital Arazi Modeli (DTM)")
 
                 self._make_centered_image(
                     os.path.join(self.output_path, "dsm_gradient.png"), 4
